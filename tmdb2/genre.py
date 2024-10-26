e2528581744661d9727dee93518dacdbe1cf75d4
EsraKorkmazz <esrasahinq@icloud.com>
2024-10-26 23:57:48 +0300

    update

--- tmdb2/genre.py
+++ tmdb2/genre.py
@@ -1,52 +1,55 @@
 import requests
 import pandas as pd
 import streamlit as st
-from dotenv import load_dotenv
-import os
+import time
 
-load_dotenv() 
-TMDB_API_KEY = os.getenv("TMDB_API_KEY") 
 TMDB_BASE_URL = "https://api.themoviedb.org/3"
-
-if TMDB_API_KEY is None:
-    raise ValueError("TMDB_API_KEY not found. Please check your .env file.")
-
-
+TMDB_API_KEY = st.secrets["tmdb"]["TMDB_API_KEY"]
 data = pd.read_csv('tmdb2/top_100.csv')
 
-def get_movie_poster(movie_id):
-    url = f"{TMDB_BASE_URL}/movie/{movie_id}?api_key={TMDB_API_KEY}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        poster_path = data.get('poster_path')
-        if poster_path:
-            return f"https://image.tmdb.org/t/p/w500{poster_path}"
+def get_movie_poster(movie_id, retries=3, delay=5):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={TMDB_API_KEY}"
+    for attempt in range(retries):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                poster_path = data.get('poster_path')
+                if poster_path:
+                    return f"https://image.tmdb.org/t/p/w500{poster_path}"
+        except requests.ConnectionError:
+            print(f"Connection error. Retrying in {delay} seconds...")
+            time.sleep(delay)
     return None
 
-def recommend_movies_by_user_genre(selected_genre, movies_df, top_n=30):
-    movies_df['genres'] = movies_df['genres'].fillna('')
-    if movies_df['genres'].dtype == object:
-        movies_df['genres'] = movies_df['genres'].apply(lambda x: x.split(',') if isinstance(x, str) else [])
-
-    filtered_movies = movies_df[movies_df['genres'].apply(lambda genres: selected_genre in genres)]
+def recommend_movies_by_user_genre(selected_genre, data, top_n=30):
+    data['genres'] = data['genres'].fillna('')
+    if data['genres'].dtype == object:
+        data['genres'] = data['genres'].apply(lambda x: x.split(',') if isinstance(x, str) else [])
+    filtered_movies = data[data['genres'].apply(lambda genres: selected_genre in genres)]
     recommended_movies = filtered_movies.sort_values(by='rating', ascending=False).head(top_n)
     return recommended_movies
 
 def main():
-    selected_genre = st.selectbox("Select a genre to get movie recommendations:", options=["Action", "Comedy", "Drama", "Horror", "Romance", "Science Fiction", "Thriller", "Family", "Crime", "Fantasy"])
+    st.title("Movie Recommendations by Genre")
+    selected_genre = st.selectbox("Select a genre to get movie recommendations:", 
+                                options=["Action", "Comedy", "Drama", "Horror", "Romance", 
+                                            "Science Fiction", "Thriller", "Family", "Crime", "Fantasy"])
 
     if st.button("Get Recommendations"):
         recommended_movies = recommend_movies_by_user_genre(selected_genre, data)
+        st.session_state['recommended_movies'] = recommended_movies
+        st.session_state['selected_genre'] = selected_genre 
+        st.write(f"Recommendations loaded for genre: {selected_genre}")
 
-        st.subheader(f"Top {len(recommended_movies)} Movies in {selected_genre} Genre")
-        cols = st.columns(3) 
-
+    if 'recommended_movies' in st.session_state:
+        recommended_movies = st.session_state['recommended_movies']
+        st.subheader(f"Top {len(recommended_movies)} Movies in {st.session_state['selected_genre']} Genre")
+        cols = st.columns(3)
         for index, row in recommended_movies.iterrows():
             poster_url = get_movie_poster(row['movie_id'])
             tmdb_link = f"https://www.themoviedb.org/movie/{row['movie_id']}"
-
-            with cols[index % 3]: 
+            with cols[index % 3]:
                 st.subheader(row['title'])
                 st.subheader(f"Rating: {row['rating']:.1f}")
                 if poster_url:
@@ -57,5 +60,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
